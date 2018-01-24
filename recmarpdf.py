fde9d40d6276f96332ba574ced871c9fdffe6f2f
Hagen Paul Pfeifer <hagen@jauu.net>
2018-01-24 21:14:55 +0100

    add flat-out option
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- recmarpdf.py
+++ recmarpdf.py
@@ -2,6 +2,8 @@
 
 import os
 import subprocess
+import argparse
+import sys
 
 
 def sanity_file(path, filename):
@@ -22,25 +24,38 @@ def sanity_file(path, filename):
 def execute(cmd):
     subprocess.call(cmd.split())
 
-def process_document(path, filename):
+def process_document(args, path, filename):
     print('process {}'.format(os.path.join(path, filename)))
-    pdf_out_path = os.path.join(path, "{}.pdf".format(filename[0:-3]))
+    if args.flat_out:
+        pdf_out_path = os.path.join(args.flat_out, "{}.pdf".format(filename[0:-3]))
+    else:
+        pdf_out_path = os.path.join(path, "{}.pdf".format(filename[0:-3]))
     new = sanity_file(path, filename)
     print(' generate PDF {}'.format(pdf_out_path))
     cmd = 'pandoc {} -o {}'.format(new, pdf_out_path)
     execute(cmd)
     os.remove(new)
 
-def process_dir(path):
+def process_dir(args, path):
     for root, dirs, files in os.walk(path):
         path = root.split(os.sep)
         for filename in files:
             if not filename.endswith('.md'):
                 continue
-            process_document(root, filename)
+            process_document(args, root, filename)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--flat-out", nargs='?', type=str, default=None)
+    args = parser.parse_args()
+    return args
 
 def main():
-    process_dir(".")
+    args = parse_args()
+    if args.flat_out and not os.path.exists(args.flat_out):
+        print('generate directory {}'.format(args.flat_out))
+        os.makedirs(args.flat_out)
+    process_dir(args, ".")
 
 if __name__ == '__main__':
   main()
